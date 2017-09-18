1ad1d2ba880dbcde5e512f17d04c7386a0637d78
Jianfa <buptljf@gmail.com>
2017-09-18 11:21:04 -0700

    Add 22.py and update README.md

---
+++ Medium/22.py
@@ -0,0 +1,65 @@
+# ------------------------------
+# 22. Generate Parentheses
+# 
+# Description:
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+# For example, given n = 3, a solution set is:
+# [
+#   "((()))",
+#   "(()())",
+#   "(())()",
+#   "()(())",
+#   "()()()"
+# ]
+# 
+# Version: 1.0
+# 09/18/17 by Jianfa
+# ------------------------------
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        if n == 0:
+            return [""]
+        elif n == 1:
+            return ["()"]
+        elif n == 2:
+            return ["(())", "()()"]
+        else:
+            mid = n // 2
+            temp_list = []
+
+            for i in range(1, mid+1):
+                temp1 = self.generateParenthesis(i)
+                temp2 = self.generateParenthesis(n-i)
+                for t1 in temp1:
+                    for t2 in temp2:
+                        if i == 1:
+                            temp_list.append("(" + t2 + ")")
+                        else:
+                            pass
+                        if t1 != t2:
+                            temp_list.append(t1+t2)
+                            temp_list.append(t2+t1)
+                        else:
+                            temp_list.append(t1+t2)                    
+
+            return list(set(temp_list))
+
+
+# Used for test
+# if __name__ == "__main__":
+#     test = Solution()
+#     n = 4
+    
+#     print(test.generateParenthesis(n))
+
+# ------------------------------
+# Summary:
+# At first I think there are only three combination: 
+# ["("+generateParenthesis(n-1)+")", "()"+generateParenthesis(n-1), generateParenthesis(n-1)+"()"]
+# But there is another situation. For example when n=4, there is a combination "(())(())", 
+# which cannot fit in anyone I thought before.
--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Leetcode
 
 ## Finished questions
+### Easy
 | # | Title	| Difficulty | Beats | Version |
 | -------- | ------------- | ---------- | ------------ | ------------- |
 | 13 | Roman to Integer | Easy | 89.60% | [1](https://github.com/Hellofafar/Leetcode/blob/master/Easy/13.py) |
 | 14 | Longest Common Prefix | Easy | 29.30% | [1](https://github.com/Hellofafar/Leetcode/blob/master/Easy/14.py) |
 | 20 | Valid Parentheses | Easy | 61.05% | [1](https://github.com/Hellofafar/Leetcode/blob/master/Easy/20.py) |
 | 21 | Merge Two Sorted Lists | Easy | 39.35% | [1](https://github.com/Hellofafar/Leetcode/blob/master/Easy/21.py)
+
+### Medium
+| # | Title	| Difficulty | Beats | Version |
+| -------- | ------------- | ---------- | ------------ | ------------- |
+| 22 | Generate Parentheses | Medium | 56.01% | [1](https://github.com/Hellofafar/Leetcode/blob/master/Medium/22.py)
